5431cf4f4362de06801f05b9c866fb049562a35b
Rafaela Rodrigues <rafaelarodrigues@jungledevs.com>
2022-05-22 00:10:25 -0300

    styles: Add globals css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import Head from 'next/head'
+
 export default function Home() {
-  return <div>hello</div>
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+    </>
+  )
 }
