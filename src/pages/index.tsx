0b8d59324a2debe2a8074bbf6b56ecebbfd057b7
Rafaela Rodrigues <rafaelarodrigues@jungledevs.com>
2022-05-29 22:05:28 -0300

    feat: Add page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
 import Head from 'next/head'
+import Image from 'next/image'
+
+import girlCoding from '../../public/images/avatar.svg'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
+
+      <main className={styles.container}>
+        <section className={styles.content}>
+          <span className={styles.text}>👏 Hey, welcome</span>
+          <h1 className={styles.title}>
+            News about <br /> the <span>React</span> world.
+          </h1>
+          <p className={styles.text}>
+            Get access to all publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <div className={styles.logo}>
+          <Image src={girlCoding} alt='Girl coding' />
+        </div>
+      </main>
     </>
   )
 }
