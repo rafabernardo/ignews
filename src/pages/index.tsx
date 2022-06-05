e1a0f5ed62a3ddc9a9cf4d31684b107dd4cd991e
Rafaela Rodrigues <rafaelarodrigues@jungledevs.com>
2022-06-04 22:38:50 -0300

    feat: Add static generation to get price details

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 
 import { stripe } from '../services/stripe'
 import HomeContent from '../components/home'
@@ -7,7 +7,6 @@ import { HomeProps } from '../types/home'
 import { getStripeCurrency } from '../utils/helpers'
 
 export default function Home({ product }: HomeProps) {
-  console.log('🚀 ~ file: index.tsx ~ line 10 ~ Home ~ product', product)
   return (
     <>
       <Head>
@@ -18,11 +17,10 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve(process.env.STRIPE_SUBSCRIPTION_PRICE_ID, {
     expand: ['product'],
   })
-  console.log('🚀 ~ file: index.tsx ~ line 25 ~ constgetServerSideProps:GetServerSideProps= ~ price', price)
 
   const product = {
     priceId: price.id,
@@ -32,5 +30,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   return {
     props: { product },
+    revalidate: 60 * 60 * 24,
   }
 }
