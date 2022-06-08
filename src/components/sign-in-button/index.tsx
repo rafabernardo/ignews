f014321ca363222c89ce5a2b58b9fe089ec49748
Rafaela Rodrigues <rafaelarodrigues@jungledevs.com>
2022-06-07 23:30:10 -0300

    feat: Add github integration

--- src/components/sign-in-button/index.tsx
+++ src/components/sign-in-button/index.tsx
@@ -1,17 +1,27 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import classnames from 'classnames'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 import styles from './styles.module.scss'
 
 const SignInButton = ({ className }) => {
-  const isLoggedIn = true
+  const { data: session } = useSession()
+
+  const handleClick = () => {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    signOut()
+  }
 
   return (
-    <button type='button' className={classnames(styles.button, className)}>
-      <FaGithub className={classnames(styles.icon, { [styles.logged]: isLoggedIn })} />
-      {isLoggedIn ? 'Rafaela Rodrigues' : 'Sign in with Github'}
-      {isLoggedIn && <FiX className={styles.close} />}
+    <button type='button' className={classnames(styles.button, className)} onClick={handleClick}>
+      <FaGithub className={classnames(styles.icon, { [styles.logged]: session })} />
+      {session ? session.user.name : 'Sign in with Github'}
+      {session && <FiX className={styles.close} />}
     </button>
   )
 }
